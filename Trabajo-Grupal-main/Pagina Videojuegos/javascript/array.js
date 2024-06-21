2a61d6f62c3225fda203b174dcfe5f5ea9fc0948
Nora <162056222+Norax4@users.noreply.github.com>
2024-06-20 21:55:28 -0300

    Add files via upload

---
+++ Trabajo-Grupal-main/Pagina Videojuegos/javascript/array.js
@@ -0,0 +1,44 @@
+import { User } from "./clases";
+import { Juego } from "./clases";
+
+export let juegos =[
+    new Juego("images/lords of the fallen.jpeg","Lords of the Fallen", 1, 1200),
+    new Juego("images/sao game.jpeg","Sword Art Online", 2, 450),
+    new Juego("images/forza motor.jpeg","Forza Motorsport", 3,350),
+    new Juego("images/overpass2.jpeg","Overpass", 4, 300),
+    new Juego("images/motoGP.jpeg", "MotoGP24", 5, 500),
+    new Juego("images/horizon.jpeg" ,  "Horizon Zero Dawn", 6, 450),
+    new Juego("images/hogwarts.jpeg" ,  "Hogwarts legacy",7, 600),
+    new Juego("images/biomutant.jpeg" , "Biomutan", 8,  750),
+    new Juego("images/all star.jpeg" ,  "DreamWorks",9, 400),
+    new Juego("images/dungeons.jpeg",  "Dungeons", 10, 780),
+    new Juego("images/pes2021.jpeg",  "Football 2021", 11, 900),
+    new Juego("images/forzah5.jpeg",  "Forza horizon", 12, 600),
+    new Juego("images/gta5.jpeg",  "Grand therft auto 5",  13, 600),
+    new Juego("images/mafia.jpeg",  "Mafia edicion definitiva", 14, 650),
+    new Juego("images/avengers.jpeg",  "Marvels Avengrs", 15,850),
+    new Juego("images/metro.jpeg", "Metro Exodus",  16, 500),
+    new Juego("images/naruto.jpeg", "Naruto",  17,  500),
+    new Juego("images/spiderman.jpeg",  "Spider Man",  19,  800),
+    new Juego("images/tlou1.jpeg","The last of us",21, 450),
+    new Juego("images/sims4.jpeg","The Sims 4",22, 900),
+    new Juego("images/alaskan road truckers.jpeg", "Alaskan Road Truckers", 24,400),
+    new Juego("images/skylines2.jpeg", "Cities Skylines II", 25, 600),
+    new Juego("images/hotweels unleashed.jpeg","Hot Wheels", 27, 900),
+    new Juego("images/smurfs2.jpeg", "The Smurfs 2", 28, 850),
+    new Juego("images/aliens dark descent.jpeg", "Aliens Dark Descent", 29, 750),
+    new Juego("images/gylt.jpeg", "GYLT",  30, 800),
+    new Juego("images/polbridge3.jpeg", "Poly Bridge 3",  31, 750),
+    new Juego("images/ratchetclank.jpeg","Ratchet and Clank", 32, 650),
+    new Juego("images/wanted dead.jpeg",  "Wanted Dead",  34, 600),
+    new Juego("images/isle of man.jpeg", "TT", 35, 850),
+    new Juego("images/mc legends.jpeg", "Minecraf Legens", 36, 900),
+    new Juego("images/gollum.jpeg", "Gollum", 37,  650)
+    ];
+
+export let users = [
+    new User(0, "Ana"),
+    new User(1, "Jose"),
+    new User(2, "Maria")
+    ];
+
